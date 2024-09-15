726e331563d6213b01a207bf4dfc1f8a2bbfabad
Daniel <mdanielprashanth@gmail.com>
2024-09-15 21:20:49 +0530

    stack implementation

---
+++ ds/stack.js
@@ -0,0 +1,43 @@
+// The Stack follows the principle of FILO - (FIRST IN LAST OUT )
+
+class Stack {
+    constructor() { 
+       this.array = []; 
+    }
+    
+    push(element) {
+       this.array.push(element);    
+    }
+
+    remove(){
+      if(this.array.length === 0) return console.log(`The array is empty`) 
+      this.array.shift(this.array[0])  
+    }
+
+    printArray(){
+        console.log(this.array)
+    }
+
+}
+
+const stackObj = new Stack();
+
+stackObj.push(2);
+stackObj.push(3);
+stackObj.push(4);
+
+stackObj.printArray();
+
+stackObj.remove();
+
+stackObj.printArray();
+
+stackObj.remove();
+
+stackObj.printArray();
+
+stackObj.remove();
+stackObj.printArray();
+
+
+stackObj.remove();
