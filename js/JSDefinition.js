86cd15ad772a4d8ec42553fbc32c6d4dbb89f88f
Daniel <mdanielprashanth@gmail.com>
2024-09-18 20:47:43 +0530

    js definition (execution context )

---
+++ js/JSDefinition.js
@@ -0,0 +1,10 @@
+// * Js is a Syncronus single threaded langauge for execution. Which means we cant go to line 2 untill line 1 is executed.
+
+// * Everthying inside js happens inside the execution context 
+
+// * Excecution Context is made up of two components:-
+
+// ? 1. Memory - (Variable environment)
+// ? 2. Code - (Thread of execution)
+
+// executuion context visula represenation link :- https://miro.medium.com/v2/resize:fit:4800/format:webp/1*lwJbwPn-GFJQw1PFvFSmAQ.gif
